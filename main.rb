4766f2315e4885bd253d2d2d698ac914032b3605
Denis Druzhinin <52042953+squaden@users.noreply.github.com>
2019-06-22 23:06:46 +0300

    Update main.rb

--- main.rb
+++ main.rb
@@ -1,15 +1,13 @@
-require_relative "game.rb"
-require_relative "result_printer.rb"
-require_relative "word_reader.rb"
-
-current_path = File.dirname(__FILE__)
-
-printer = ResultPrinter.new
+require_relative "game"
+require_relative "result_printer"
+require_relative "word_reader"
 
 puts "Игра виселица.\n\n"
 
+printer = ResultPrinter.new
 reader = WordReader.new
 
+current_path = File.dirname(__FILE__)
 slovo = reader.read_from_file(current_path + "/data/words.txt")
 
 game = Game.new(slovo)
